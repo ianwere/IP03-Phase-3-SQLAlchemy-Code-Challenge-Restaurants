ed6650a1e57c47886bcd8f41f47878af4198d44a
Ian Were <ian.were@student.moringaschool.com>
2023-12-11 17:52:04 +0300

    finished code challenge

--- README.md
+++ README.md
@@ -1,1 +1,59 @@
-# IP03-Phase-3-SQLAlchemy-Code-Challenge-Restaurants
+# restaurants-with-sqlalchemy
+# SQLAlchemy-Code-Challenge-Restaurants
+
+
+## Introduction
+
+This repo contains code for a restaurant review system for restuarants-code-challenge-sqlalchemy based on python classes and instances, class and instance methods, variable scope, object relationships, and lists and list methods working with  sqlalchemy.
+
+This project utilizes the `pytest` library for testing purposes. The `pytest framework` makes it easy to write small, readable tests, and can scale to support complex functional testing for applications and libraries.
+
+
+## Table of Contents
+* Technologies used
+* Installation
+* Usage
+* Author
+
+## Technologies used
+* Python
+* SQLAlchemy
+* Faker
+* Alembic
+
+## Installation
+
+### 1. Clone the repository
+
+```bash
+git clone https://github.com/faith-kaburu/restaurants-with-sqlalchemy.git
+```
+
+### 2. Navigate to the project's directory
+
+```bash
+cd  restaurants-with-sqlalchemy
+```
+
+### 3. Install all the required dependencies
+
+The root directory of this repository contains the `Pipfile` with all the required Python libraries for this project and restricts them to this repository.
+
+To install `pytest` and any other required libraries, run:
+
+```python
+pipenv install
+```
+
+### 4. Enter the pipenv shell
+
+```python
+pipenv shell
+```
+
+
+## Usage
+
+* Website contains tables for restaurants reviews and customers.
+* The methods for the restaurant,reviews and customers models are in the `models.py` file.
+* To test the methods enter into the pipenv shell and ensure that you are in `lib` directory then run `python3 debug.py`.
---
+++ seed.py
@@ -0,0 +1,72 @@
+from faker import Faker
+import random
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from models import Restaurant, Review, Customer
+
+if __name__ == '__main__':
+    
+    engine = create_engine('sqlite:///restaurants.db')
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    
+    # session.query(Restaurant).delete()
+    # session.query(Review).delete()
+    # session.query(Customer).delete()
+    
+    fake = Faker()
+    
+    names = ['Cafe Deli', 'Sarova', 'Stanley', ' Pavilio', 
+             'Sippin', 'Mint & Salt', 'Serenade', 'Del Ruiz', 'Hillton', 
+             'Radiator','Hwiralang', 'Seychelles', 'Miami', 'Massai lounge' ]
+    
+    
+    restaurants = []
+    for item in range (50):
+        restaurant = Restaurant(
+            name = random.choice(names),
+            price = random.randint(200, 500)
+        )
+        
+        # add and commit individually to get IDs back
+        session.add(restaurant)
+        session.commit()
+        
+        restaurants.append(restaurant)
+        
+    customers = []
+    for i in range(50):
+        customer = Customer(
+            first_name = fake.first_name(),
+            last_name = fake.last_name(),
+        )
+        session.add(customer)
+        session.commit()
+        
+        customers.append(customer)
+        
+        
+        
+    reviews = []
+    for restaurant in restaurants:
+        for item in range(random.randint(1,5)):
+            customer = random.choice(customers)
+            if restaurant not in customer.restaurants:
+                customer.restaurants.append(restaurant)
+                session.add(customer)
+                session.commit()
+            
+            review = Review(
+                star_rating = random.randint(0,10),
+                restaurant_id = restaurant.id,
+                customer_id = customer.id,
+            )
+            reviews.append(review)
+    
+    
+    session.bulk_save_objects(reviews)
+    session.commit()
+    session.close()
+
+    
